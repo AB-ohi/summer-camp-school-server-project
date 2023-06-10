64fb86d9f2b23eb67ae6a0ae93c92a61f59f4179
AB-ohi <askohi63907@gmail.com>
2023-06-10 20:35:54 +0600

    use jset

--- index.js
+++ index.js
@@ -31,13 +31,43 @@ async function run() {
     const classSelectCollection = client.db("Melody-School").collection("classSelect");
     const usersCollection = client.db("Melody-School").collection("users");
 
+    const verifyJWT = (req, res, next) => {
+      const authorization = req.headers.authorization;
+      if (!authorization) {
+        return res
+          .status(401)
+          .send({ error: true, massage: "unauthorized access" });
+      }
+      const token = authorization.split(" ")[1];
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res
+            .status(401)
+            .send({ error: true, massage: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
     app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h",
-      });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, );
 
       res.send({token});
+    });
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
     });
     app.get("/Instructors", async(req,res)=>{
       const result = await InstructorsCollection.find().toArray();
@@ -52,10 +82,17 @@ async function run() {
       res.send(result);
     })
 
-    app.get("/classSelect", async(req, res)=>{
+    app.get("/classSelect",verifyJWT, async(req, res)=>{
       const email = req.query.email;
       if(!email){
           res.send([])
+      }
+      
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(401)
+          .send({ error: true, message: "provident access" });
       }
       const query ={email:email}
       const result = await classSelectCollection.find(query).toArray()
@@ -79,7 +116,7 @@ async function run() {
 
    //user aip
 
-   app.get("/users", async (req, res) => {
+   app.get("/users", verifyJWT, async (req, res) => {
     const result = await usersCollection.find().toArray();
     res.send(result);
   });
