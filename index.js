0ccfe031a6571e6bb8870d4b65b0b9f5e8cd3500
AB-ohi <askohi63907@gmail.com>
2023-06-11 17:32:41 +0600

    last commit

--- index.js
+++ index.js
@@ -149,6 +149,7 @@ async function run() {
     const result = await usersCollection.insertOne(user);
     res.send(result);
   });
+  // user-admin
   app.patch("/users/admin/:id", async (req, res) => {
     const id = req.params.id;
     const filter = { _id: new ObjectId(id) };
